329a05a476120bc44786e34a02110de0f502e03f
Drew Cannedy <dxc142830@utdallas.edu>
2019-07-11 11:29:17 -0500

    added scene changing collider

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public GameObject loadingImage;
+    [SerializeField]
+    int scene = 0;
+    [SerializeField]
+    float delay = 0;
+
+    void OnTriggerEnter(Collider col)
+    {
+        loadingImage.SetActive(true);
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(scene);
+    }
+}
